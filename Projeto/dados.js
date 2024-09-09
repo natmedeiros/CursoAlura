37a38e57dabeefbf1446ad984ea0e4258120410d
natalia <nataliamaria8686@gmail.com>
2024-09-09 15:33:17 -0300

    Complemento

--- Projeto/dados.js
+++ Projeto/dados.js
@@ -2,19 +2,22 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "A Fadinha, como é carinhosamente conhecida, é a mais jovem medalhista olímpica da história do Brasil. Com apenas 13 anos, conquistou a prata nos Jogos Olímpicos de Tóquio, revolucionando o skate feminino no país.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     }, 
 
     {
     titulo: "Rebeca Andrade",
     descricao: "Ginasta brasileira que conquistou a primeira medalha de ouro olímpica na ginástica artística feminina para o Brasil. Conhecida por sua força e determinação, superou diversas lesões para alcançar o topo do pódio.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
   },
 
   {
     titulo: "Beatriz Souza",
     descricao: "Judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, tornando-se um dos maiores nomes do judô feminino brasileiro.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka"
   }
 ];
 
